b80b7effdbb8fcb0823f0454b670fdc6412b283b
tungarazaa <tungarazakennedy@gmail.com>
2025-02-03 10:04:58 +0300

    adding limiter settings

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log('connected'));
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, '0.0.0.0', () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
 
