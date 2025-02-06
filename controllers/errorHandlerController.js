46e70b77244708da012d496f4dc58b187614fd07
tungarazaa <tungarazakennedy@gmail.com>
2025-02-06 13:39:27 +0300

    Modifying error file

--- controllers/errorHandlerController.js
+++ controllers/errorHandlerController.js
@@ -50,11 +50,15 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
+    let error = { ...err, message: err.message };
     if (err.name === 'CastError') error = castErrorDB(error);
     if (err.code === 11000) error = duplicateFieldError(error);
     if (err.name === 'ValidationError') error = validationError(error);
     if (err.name === 'JsonWebTokenError') error = handleJWTError();
+
+    if (err.statusCode) error.statusCode = err.statusCode;
+    if (err.status) error.status = err.status;
+
     sendErrorProd(error, res);
   }
 };
