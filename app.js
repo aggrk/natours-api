6ffa7c2b4c6862c8a2d8aba033e4ceac3e9f5a80
tungarazaa <tungarazakennedy@gmail.com>
2025-03-04 19:15:26 +0300

    adding endpoints

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(
       'http://localhost:5173',
       'http://192.168.1.114:8081',
       'http://localhost:8081',
+      'https://natours-aggr.netlify.app',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
