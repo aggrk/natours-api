852278d0a4d903688e795d6265a8c5d84ea9e171
tungarazaa <tungarazakennedy@gmail.com>
2025-03-06 16:51:34 +0300

    Tracking activities

--- utils/email.js
+++ utils/email.js
@@ -3,7 +3,8 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   //Create a transporter
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
